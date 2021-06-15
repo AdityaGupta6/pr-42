d7d2946ce723badafcbceedc3f66127a33c256a2
Aditya <sunitagupta0776@gmail.com>
2021-06-15 16:26:08 +0530

    modified condition

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function draw() {
   if (keyCode===40) {
     spacecraft.changeAnimation("spacecraft2",spacecraftImg2)
   }
- if (spacecraft.x>=680&&spacecraft.x<=683 &&spacecraft.y<=436&&spacecraft.y>=425) {
+ if (spacecraft.x>=680&&spacecraft.x<=683 &&spacecraft.y<=436&&spacecraft.y>=430) {
    hasDocked=true
  }
 
